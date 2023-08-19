c269054aa76f8f4a777b787cdeb927c5c29bab6d
pierreviences <imaniyazid@gmail.com>
2023-08-19 19:10:28 +0700

    feat: Enhance user input handling and API integration

--- app/src/main/java/com/example/openapi/MainActivity.kt
+++ app/src/main/java/com/example/openapi/MainActivity.kt
@@ -5,14 +5,37 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val question = findViewById<EditText>(R.id.etQuestion)
+
+
+        val etQuestion = findViewById<EditText>(R.id.etQuestion)
         val btnSubmit = findViewById<Button>(R.id.btnSubmit)
         val txtResponse = findViewById<TextView>(R.id.txtResponse)
+
+        val question = etQuestion.text.toString()
+        btnSubmit.setOnClickListener {
+            Toast.makeText(this, question.toString(), Toast.LENGTH_SHORT).show()
+            getResponse(question){
+                response ->
+                runOnUiThread{
+                    txtResponse.text = response
+                }
+            }
+        }
+
+    }
+
+    fun getResponse(question: String, callback: (String) -> Unit){
+        val apikey = "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"
+        val url = "https://api.openai.com/v1/completions"
     }
 }
