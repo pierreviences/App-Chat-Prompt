f498784cac851199b2a791fd8be39b1754830d32
pierreviences <imaniyazid@gmail.com>
2023-08-19 19:01:25 +0700

    feat: Initialize UI elements in MainActivity

--- app/src/main/java/com/example/openapi/MainActivity.kt
+++ app/src/main/java/com/example/openapi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.openapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val question = findViewById<EditText>(R.id.etQuestion)
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        val txtResponse = findViewById<TextView>(R.id.txtResponse)
     }
 }
